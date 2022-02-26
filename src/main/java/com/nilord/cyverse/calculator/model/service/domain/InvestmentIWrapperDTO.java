47b608d3bb6329b6d8a91797535631ef69400268
Fernando González <oxnightlordxo@gmail.com>
2022-02-25 20:37:21 -0500

    Fix attribute descriptions

--- src/main/java/com/nilord/cyverse/calculator/model/service/domain/InvestmentIWrapperDTO.java
+++ src/main/java/com/nilord/cyverse/calculator/model/service/domain/InvestmentIWrapperDTO.java
@@ -14,10 +14,10 @@ public class InvestmentIWrapperDTO implements Serializable {
   
   private static final long serialVersionUID = 5229691231613547980L;
 
-  @Schema(description = "Backdoor fee monthly amount", example = "49.75", required = false)
+  @Schema(description = "Backdoor fee amount", example = "49.75", required = false)
   private TimeAmountWrapperDTO backdoorFee;
   
-  @Schema(description = "Server fee monthly amount", example = "300.25", required = false)
+  @Schema(description = "Server fee amount", example = "300.25", required = false)
   private TimeAmountWrapperDTO serverFee;
   
   @Schema(description = "Total amount", example = "350.00", required = false)
